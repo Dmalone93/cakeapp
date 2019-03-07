a4d5ce7220afaffd1ac92e47dcb4c05d6ee127f7
Declan Malone <declanamalone@gmail.com>
2019-03-07 18:55:42 +0000

    added in cakeContainer

--- src/containers/cakes/CakeContainer.js
+++ src/containers/cakes/CakeContainer.js
@@ -6,4 +6,18 @@ class CakeContainer extends React.Component {
     super(props);
     this.state = {cakes: []}
   }
+
+  componentDidMount(){
+    let request = new Request()
+    request.get('/api/cakes').then((data) => {
+      this.setState({cakes: data.cakes})
+    })
+  }
+
+  render(){
+    return ()
+    <CakeList cakes = {this.state.cakes} />
+  }
 }
+
+export default CakeContainer;
